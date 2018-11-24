6250e932544985ba63aa90ec4d1b29ed0164b9f7
mshtang <mshtang@outlook.com>
2018-11-24 20:20:37 +0100

    rewrite the debug routine

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-//#include "c:\Eigen\Eigen\Dense"
 #include <iostream>
 #include "InOut.h"
 #include "Sample.h"
@@ -8,83 +7,89 @@
 #include "utils.h"
 #include <chrono>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	// prepare dataset
-	// instantiate a training object
-	InOut trnObj;
-	Eigen::MatrixXf cloud;
-	//Eigen::MatrixXf points;
-	Eigen::VectorXi cloudLabels;
-	//Eigen::VectorXi pointLabels;
+	// change here to enter debug mode
+	bool debug = true;
+	// to control whether training from scratch or use a pretrained model
+	bool directTrain = false;
 
-	//trnObj.readPoints("./datasets/bildstein_station1_xyz_intensity_rgb_trainData.txt", cloud);
-	trnObj.readPoints("./toy_dataset/downsampled.txt", cloud);
-	//trnObj.readLabels("./datasets/bildstein_station1_xyz_intensity_rgb_train.labels", cloudLabels);
-	trnObj.readLabels("./toy_dataset/downsampled.labels", cloudLabels);
-
-	// search for the k nearest neighbors for each point in the dataset
-	// and store the indices and dists in two matrices for later use by
-	// indexing instead of searching again
-
-	Eigen::MatrixXi trnIndices;
-	Eigen::MatrixXf trnDists;
-	trnObj.searchNN(cloud, trnIndices, trnDists);
-
-	/*Sample trnSamples(&trnData, &trnLabels, &trnIndices, &trnDists, 8, 1);
-	Eigen::MatrixXf local_nei = trnSamples.buildNeighborhood(0);
-	std::cout << "Neighborhood of Point 1 is:\n" << local_nei << std::endl;
-	trnSamples.randomSampleFeatures();
-	std::vector<Features> feats = trnSamples.getSelectedFeatures();
-	std::cout << "No. of voxels are " << feats.at(0)._numVoxels << std::endl;
-	Features testFeat;
-	testFeat._featType = 16;
-	testFeat._numVoxels = 1;
-	testFeat._pointId = { 5 };
-	testFeat._voxelSize = { 7 };
-	FeatureFactory ff(local_nei, testFeat);
-
-	std::cout << "Local neighbors are:\n" << ff.getLocalNeighbors() << std::endl;
-	
-	std::vector<Eigen::Matrix3f> cov = ff.computeCovarianceMatrix();
-	for (auto i : cov)
-		std::cout << i << std::endl << std::endl;
-
-	std::vector<Eigen::Vector3f> eigv = ff.computeEigenValues(cov);
-	for (auto i : eigv)
-		std::cout << i.transpose() << std::endl << std::endl;
-	 ff.computeFeature();*/
-	
-	int numTrees = 3;
-	int maxDepths = 5;
+	// give respective dataset/label path
+	std::string trainSetPath = "./datasets/path_to_trainset.txt";
+	std::string valSetPath = "./datasets/path_to_valset.txt";
+	std::string trainLabelPath = "./datasets/path_to_train.labels";
+	std::string valLabelPath = "./datasets/path_to_val.labels";
+	// if training the real dataset, give a path to save the model
+	std::string modelPath = "./models/path_to_the_real_model.model";
+	// parameters to modify
+	int numTrees = 10;
+	int maxDepth = 10;
 	int minSamplesPerLeaf = 20;
-	//RandomForest rf(numTrees, maxDepths, minSamplesPerLeaf);
+	int featsPerNode = 15;
+
+	if (debug)
+	{
+		modelPath = "./models/test.model";
+		trainSetPath = "./toy_dataset/downsampled.txt";
+		trainLabelPath = "./toy_dataset/downsampled.labels";
+		valSetPath = "./toy_dataset/testset.txt";
+		valLabelPath = "./toy_dataset/testset_from_model.labels";
+		numTrees = 2;
+		maxDepth = 5;
+		minSamplesPerLeaf = 20;
+		featsPerNode = 5;
+	}
+
+	// in this work it's 8
 	int numClasses = 8;
-	int featsPerNode = 5;
 
-	/*auto start = std::chrono::system_clock::now();
-	rf.train(&cloud, &cloudLabels, &trnIndices, &trnDists, numClasses, featsPerNode);
-	auto end = std::chrono::system_clock::now();
-	double elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
-	std::cout << "Training takes: " << elapsed << "s" << std::endl;
-	
-	rf.saveModel("./models/test.model");*/
 
-	RandomForest rf("./models/test.model");
-	
-	
-	Eigen::VectorXi predictedLabels;
+	if (directTrain)
+	{
+		// prepare dataset
+		InOut trainObj;
+		Eigen::MatrixXf cloud;
+		Eigen::VectorXi labels;
 
-	auto start2 = std::chrono::system_clock::now();
-	//rf.predict("./datasets/bildstein_station1_xyz_intensity_rgb_valData.txt", predictedLabels);
-	rf.predict("./toy_dataset/testset.txt", predictedLabels);
-	auto end2 = std::chrono::system_clock::now();
+		// reading dataset and labels from given file path
 
-	auto elapsed2 = std::chrono::duration_cast<std::chrono::duration<double>>(end2 - start2).count();
-	std::cout << "Predicting takes: " << elapsed2 << "s" << std::endl;
-	
-	//trnObj.writeToDisk("./datasets/predict.labels", predictedLabels);
-	trnObj.writeToDisk("./toy_dataset/predict_from_restore.labels", predictedLabels);
+		trainObj.readPoints(trainSetPath.c_str(), cloud);
+		trainObj.readLabels(trainLabelPath.c_str(), labels);
+
+		Eigen::MatrixXi trainIndex;
+		Eigen::MatrixXf trainDists;
+		Eigen::VectorXi predictedLabels;
+
+		trainObj.searchNN(cloud, trainIndex, trainDists);
+		RandomForest randObj(numTrees, maxDepth, minSamplesPerLeaf);
+		auto start = std::chrono::system_clock::now();
+		randObj.train(&cloud, &labels, &trainIndex, &trainDists, numClasses, featsPerNode);
+		auto end = std::chrono::system_clock::now();
+		double elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+		std::cout << "Training takes: " << elapsed << "s" << std::endl;
+		randObj.saveModel(modelPath.c_str());
+
+		start = std::chrono::system_clock::now();
+		randObj.predict(valSetPath.c_str(), predictedLabels);
+		end = std::chrono::system_clock::now();
+		elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+		std::cout << "Predicting takes: " << elapsed << "s" << std::endl;
+
+		trainObj.writeToDisk(valLabelPath.c_str(), predictedLabels);
+	}
+	else
+	{
+		RandomForest randObj(modelPath.c_str());
+		Eigen::VectorXi predictedLabels;
+		auto start = std::chrono::system_clock::now();
+		randObj.predict(valSetPath.c_str(), predictedLabels);
+		auto end = std::chrono::system_clock::now();
+		double elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+		std::cout << "Predicting takes: " << elapsed << "s" << std::endl;
+		InOut testObj;
+		testObj.writeToDisk(valLabelPath.c_str(), predictedLabels);
+	}
+
 	system("pause");
 	return 0;
 }
